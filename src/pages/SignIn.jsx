2e4b04b391af8d5435f445921e39f686cda04023
expeditus303 <ricardotonietto@gmail.com>
2023-10-30 20:54:45 -0300

    feat: github actions for deploy on AWS

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -65,7 +65,7 @@ export default function SignIn() {
         </form>
 
         <Link to="/cadastro">
-          First time? $ign Up here!
+          First time? $ign Up here!!
         </Link>
       </SignInContainer>
     </>
